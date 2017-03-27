f7cb94bc5082370b9c219411ea917055a1e9535e
Michael Stashevsky <mstashev@gmail.com>
2017-03-27 17:10:50 -0400

    Materials resources added

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,19 +1,2 @@
 class MaterialsController < ApplicationController
-  def index
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def show
-  end
-
-  def destroy
-  end
-
-  def update
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :materials
   get 'materials/index'
 
   get 'materials/create'
