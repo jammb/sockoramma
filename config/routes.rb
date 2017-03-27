919a8609c1646e1991a89072ab32cae77f6d9dcf
Michael Stashevsky <mstashev@gmail.com>
2017-03-27 17:19:34 -0400

    Created Items resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :items
   resources :sizes
   resources :colors
   resources :styles
   resources :materials
+
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
