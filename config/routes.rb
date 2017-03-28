a6b0f82096a94b73fde671de9f17b77e1b727c8f
Ben Call <bgcall@indiana.edu>
2017-03-28 13:42:26 -0400

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :addresses
-  resources :orders
-  resources :items
-  resources :sizes
-  resources :colors
-  resources :styles
-  resources :materials
 
+  scope :api do
+    resources :users
+    resources :addresses
+    resources :orders
+    resources :items
+    resources :sizes
+    resources :colors
+    resources :styles
+    resources :materials
+  end
   
 
 end
