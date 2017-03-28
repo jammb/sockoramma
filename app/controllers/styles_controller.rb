38426c126ef05b9bada4f76303eed574a760a16b
Michael Stashevsky <mstashev@gmail.com>
2017-03-28 17:53:08 -0400

    styles actions

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,2 +1,31 @@
 class StylesController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def index
+    @styles = Style.all
+    render json: @styles
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+  def find_by_id
+    @style = Style.find_by(params['id'])
+  end
+
+  
 end
