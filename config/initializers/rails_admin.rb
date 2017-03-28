aff7bff967690caf381fb2701cc00849d259d712
Ben Call <bgcall@indiana.edu>
2017-03-28 14:35:03 -0400

    generated admin area and excluded orderings

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,11 @@
 RailsAdmin.config do |config|
 
+config.authorize_with do
+  authenticate_or_request_with_http_basic('Site Message') do |username, password|
+    User.find_by(username: username).try(:authenticate, password).try(:admin?)
+  end
+end
+
   ### Popular gems integration
 
   ## == Devise ==
@@ -18,6 +24,8 @@ RailsAdmin.config do |config|
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
+  config.excluded_models << "orderings"
 
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
