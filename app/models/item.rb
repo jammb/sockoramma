c37bf21f208e0889e28d048345e5faccee46c8c2
Ben Call <bgcall@indiana.edu>
2017-03-28 08:56:47 -0400

    Added associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :material
   belongs_to :color
   belongs_to :style
+  has_many :orders, through: :orderings
+  has_many :sizes, through: :quantities
 end
