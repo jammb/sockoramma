9abef76bd2051a5f7b05b20d48bb32fe1f49e015
Michael Stashevsky <mstashev@gmail.com>
2017-03-28 16:43:47 -0400

    updated schema to set the defaults of admin and featured to false

--- db/migrate/20170328174433_add_featured_to_items.rb
+++ db/migrate/20170328174433_add_featured_to_items.rb
@@ -1,5 +1,5 @@
 class AddFeaturedToItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :items, :featured, :boolean
+    add_column :items, :featured, :boolean, :default => false
   end
 end
