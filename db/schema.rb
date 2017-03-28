c5b35e1325f489bf27fcc8af83217dfcb90767fa
Michael Stashevsky <mstashev@gmail.com>
2017-03-28 15:40:05 -0400

    Seeded db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170328175300) do
-=======
-ActiveRecord::Schema.define(version: 20170328191257) do
->>>>>>> Seeded db
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
