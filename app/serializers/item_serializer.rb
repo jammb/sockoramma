05048e0fa316ebc5383aebf959605c4409543f7c
Ben Call <bgcall@indiana.edu>
2017-03-29 08:29:14 -0400

    Finished the serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :picture, :price
+  attributes :id, :title, :description, :picture, :price, :featured
   belongs_to :material
   belongs_to :color
   belongs_to :style
