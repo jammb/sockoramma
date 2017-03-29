575691ccfe6129e1b68530eff9c71217bccd37df
Michael Stashevsky <mstashev@gmail.com>
2017-03-29 16:28:18 -0400

    fixed seeds.rb to only print quantities of socks in sizes

--- app/serializers/size_serializer.rb
+++ app/serializers/size_serializer.rb
@@ -4,7 +4,7 @@ class SizeSerializer < ActiveModel::Serializer
   has_many :items, through: :quantities
 
   def quantity
-    object.items.quantities.first.quantity
+    object.items.first.quantities.first.quantity
   end
 
 end
