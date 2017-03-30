3ef3e046ed0dab0b439b03bb9953295eb59d4a58
Michael Stashevsky <mstashev@gmail.com>
2017-03-30 14:54:30 -0400

    fixed migration

--- db/migrate/20170330135503_create_carts.rb
+++ db/migrate/20170330135503_create_carts.rb
@@ -2,7 +2,7 @@ class CreateCarts < ActiveRecord::Migration[5.0]
   def change
     create_table :carts do |t|
       t.string :token
-      t.references :user_id, foreign_key: true
+      t.references :user, foreign_key: true
       t.string :status
 
       t.timestamps
