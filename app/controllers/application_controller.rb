888da57ae1ea13d181bb51eaa217546ed2209415
Michael Stashevsky <mstashev@gmail.com>
2017-03-30 10:14:38 -0400

    create Cart and LineItem resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
 
   def static
-    render file: "public/index.html"
+    render html: File.open("#{Rails.root}/public/index.html").read.html_safe, status: 200
   end
 end
