b6b38a4be3df9939feefbf38a66576c644e9a4fd
Ben Call <bgcall@indiana.edu>
2017-03-31 13:57:12 -0400

    rendereing userexpanded on session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      # redirect_to :root
+      render json: @user, serializer: UserExpandedSerializer
     else
       flash[:danger] = "Incorrect credentials. Please try again."
       # redirect_to :login
