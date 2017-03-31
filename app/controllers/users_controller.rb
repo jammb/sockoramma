3e994ec526e4c7115002792bb27e6b375124bb93
Ben Call <bgcall@indiana.edu>
2017-03-31 13:25:41 -0400

    fixed mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.signup(@user).deliver
+      session[:user_id] = @user.id
       render json: @user, serializer: UserExpandedSerializer
     else
       render json: @user.errors.full_messages, status: 400
